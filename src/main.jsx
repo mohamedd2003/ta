b2caab6a14241151e0765b83cbc8964ba7c75e9c
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2024-01-30 00:53:29 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'preact'
-import App from './Components/App/App';
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import  '@fortawesome/fontawesome-free/css/all.min.css'
-
-render(<App />, document.getElementById('app'))
