77adc63656cb64bb4938883dc1af1fe952d85fd3
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2024-01-31 00:00:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,6 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import  '@fortawesome/fontawesome-free/css/all.min.css'
-import "@vercel/analytics"
+
 
 render(<App />, document.getElementById('app'))
